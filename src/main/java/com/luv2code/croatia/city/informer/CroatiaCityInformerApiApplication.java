933ac074bf82ebb36a854cefdc476c865d74e094
lzugaj <luka.zugaj@sigit.hr>
2024-05-07 16:49:03 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/croatia/city/informer/CroatiaCityInformerApiApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.croatia.city.informer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroatiaCityInformerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CroatiaCityInformerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv2code/croatia/city/informer/CroatiaCityInformerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.croatia.city.informer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroatiaCityInformerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
